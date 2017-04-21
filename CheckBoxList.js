d98d3c87cc34c621dfade2cd47523dca77d52809
Kory Becker <kbecker@primaryobjects.com>
2017-04-21 10:12:11 -0400

    Removed reference to React.createClass (deprecated) and replaced with class CheckBoxList. Updated to Bootstrap v4 alpha.

--- CheckBoxList.js
+++ CheckBoxList.js
@@ -1,83 +1,72 @@
 'use strict';
 var React = require('react');
 
-module.exports = React.createClass({
-	displayName: 'CheckBoxList',
+module.exports = class CheckBoxList extends React.Component {
+  constructor(props) {
+    super(props);
 
-	propTypes: {
-		defaultData: React.PropTypes.array,
-		onChange: React.PropTypes.func
-	},
+    this.state = {
+        data: props.defaultData || []
+    };
 
-	getInitialState: function() {
-		return {
-			data: this.props.defaultData || []
-		};
-	},
+    this.handleItemChange = this.handleItemChange.bind(this);
+    this.reset = this.reset.bind(this);
+  }
 
-	handleItemChange: function(e) {
-		var selectedValues = [],
-			newData = [];
+  handleItemChange(e) {
+    var selectedValues = [],
+        newData = [];
 
-		this.state.data.forEach(function(item) {
-			if(item.value === e.target.value) {
-				item.checked = e.target.checked;
-			}
-			if(item.checked) {
-				selectedValues.push(item.value);
-			}
-			newData.push(item);
-		});
+    this.state.data.forEach(function(item) {
+      if(item.value === e.target.value) {
+          item.checked = e.target.checked;
+      }
+      if(item.checked) {
+          selectedValues.push(item.value);
+      }
+      newData.push(item);
+    });
 
-		this.setState({data: newData});
+    this.setState({data: newData});
 
-		if(this.props.onChange) {
-			this.props.onChange(selectedValues);
-		}
-	},
+    if(this.props.onChange) {
+      this.props.onChange(selectedValues);
+    }
+  }
 
-	// uncheck all items in the list
-	reset: function() {
-		var newData = [];
-		this.state.data.forEach(function(item) {
-			item.checked = false;
-			newData.push(item);
-		});
+  // uncheck all items in the list
+  reset() {
+    var newData = [];
+    this.state.data.forEach(function(item) {
+      item.checked = false;
+      newData.push(item);
+    });
 
-		this.setState({data: newData});
-	},
-	
-	checkAll: function() {
-		var newData = [];
-		this.state.data.forEach(function(item) {
-			item.checked = true;
-			newData.push(item);
-		});
+    this.setState({data: newData});
+  }
 
-		this.setState({data: newData});
-	},
+  render() {
+    var options;
 
-	render: function() {
-		var options;
+    options = this.state.data.map(function(item, index) {
+      return (
+        React.createElement('div', {key: 'chk-' + index, className: 'form-check'}, 
+          React.createElement('label', {className: 'form-check-label'}, 
+            React.createElement('input', {
+              type: 'checkbox', 
+              className: 'form-check-input',
+              value: item.value, 
+              onChange: this.handleItemChange, 
+              checked: item.checked ? true : false}), ' ', item.label
+          )
+        )
+      );
+    }.bind(this));
 
-		options = this.state.data.map(function(item, index) {
-			return (
-				React.createElement("div", {key: 'chk-' + index, className: "checkbox"}, 
-					React.createElement("label", null, 
-						React.createElement("input", {
-							type: "checkbox", 
-							value: item.value, 
-							onChange: this.handleItemChange, 
-							checked: item.checked ? true : false}), " ", item.label
-					)
-				)
-			);
-		}.bind(this));
-
-		return (
-			React.createElement("div", null, 
-				options
-			)
-		);
-	}
-});
+    return (
+      React.createElement('div', null, 
+          options
+      )
+    );
+  }
+}
